21aab8a3f87804a91a38bdfd5f737b83ffb5526a
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-22 04:44:02 -0800

    Adding GetUserTokenInfoOperation

---
+++ src/main/java/tech/aroma/banana/authentication/service/operations/GetUserTokenInfoOperation.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.aroma.banana.authentication.service.operations;
+
+
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.banana.authentication.service.AuthenticationAssertions;
+import tech.aroma.banana.thrift.authentication.service.GetUserTokenInfoRequest;
+import tech.aroma.banana.thrift.authentication.service.GetUserTokenInfoResponse;
+import tech.sirwellington.alchemy.annotations.access.Internal;
+import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
+
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Internal
+final class GetUserTokenInfoOperation implements ThriftOperation<GetUserTokenInfoRequest, GetUserTokenInfoResponse>
+{
+    private final static Logger LOG = LoggerFactory.getLogger(GetUserTokenInfoOperation.class);
+
+    @Override
+    public GetUserTokenInfoResponse process(GetUserTokenInfoRequest request) throws TException
+    {
+        AuthenticationAssertions.checkRequestNotNull(request);
+        
+        LOG.debug("Received request to get token info: {}", request);
+        
+        GetUserTokenInfoResponse response = one(pojos(GetUserTokenInfoResponse.class));
+        
+        return response;
+    }
+
+}
