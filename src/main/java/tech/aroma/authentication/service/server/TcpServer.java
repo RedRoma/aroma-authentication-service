fdfc397040451551406380972042056a8644559e
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-03-02 09:31:49 -0800

    TcpServer: Updating Port Logic

--- src/main/java/tech/aroma/authentication/service/server/TcpServer.java
+++ src/main/java/tech/aroma/authentication/service/server/TcpServer.java
@@ -19,9 +19,12 @@ package tech.aroma.authentication.service.server;
 
 
 
+import com.google.common.base.Preconditions;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.net.SocketException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
@@ -52,6 +55,9 @@ public final class TcpServer
 
     public static void main(String[] args) throws TTransportException, SocketException
     {
+        int port = getPortFromArgs(args);
+        
+        
         Injector injector = Guice.createInjector(new ModuleAuthenticationOperations(),
                                                  new ModuleAuthenticationService(),
                                                  new ModuleCassandraDataRepositories(),
@@ -60,7 +66,7 @@ public final class TcpServer
         AuthenticationService.Iface authenticationService = injector.getInstance(AuthenticationService.Iface.class);
         AuthenticationService.Processor processor = new AuthenticationService.Processor<>(authenticationService);
 
-        TServerSocket socket = new TServerSocket(DEFAULT_PORT);
+        TServerSocket socket = new TServerSocket(port);
         socket.getServerSocket().setSoTimeout((int) SECONDS.toMillis(30));
 
         TThreadPoolServer.Args serverArgs = new TThreadPoolServer.Args(socket)
@@ -71,10 +77,35 @@ public final class TcpServer
             .minWorkerThreads(5)
             .maxWorkerThreads(100);
         
-        LOG.info("Starting Authentication Service at port {}", DEFAULT_PORT);
+        LOG.info("Starting Authentication Service at port {}", port);
         
         TThreadPoolServer server = new TThreadPoolServer(serverArgs);
         server.serve();
         server.stop();
+    }
+
+    private static int getPortFromArgs(String[] args)
+    {
+        OptionParser parser = new OptionParser();
+        parser.accepts("port").withRequiredArg();
+        OptionSet options = parser.parse(args);
+
+        Integer port = DEFAULT_PORT;
+        if (!options.has("port"))
+        {
+            return port;
+        }
+
+        try
+        {
+            port = Integer.valueOf(options.valueOf("port").toString());
+            Preconditions.checkArgument(port > 0, "Port must be at least 0");
+        }
+        catch (Exception ex)
+        {
+            LOG.warn("Bad --port argument. Using default {}", port, ex);
+        }
+
+        return port;
     }
 }
